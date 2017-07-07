8b3acd33aeb7b635b9d62dfc2a9f22f3c6ccef9d
Alexandru Ghirua <ghalex@gmail.com>
2017-07-07 15:52:25 +0300

    Add style-components babel plugin

--- src/AutocompleteLocation/styles.js
+++ src/AutocompleteLocation/styles.js
@@ -1,10 +1,11 @@
 import styled from 'styled-components'
+import { font } from 'styled-theme'
 
 export const AutocompleteLocationStyle = styled.div`
   & input {
     width: 100%;
     box-sizing: border-box;
-    font-family: ${p => p.theme.fontFamily};
+    font-family: ${font('primary')};
     font-size: 14px;
     border: 1px solid #D8D8D8;
     border-radius: 4px;
