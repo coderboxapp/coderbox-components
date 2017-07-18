f80a70c3ade8506f04d2d7467d219108eba83064
Alexandru Ghiur <ghalex@gmail.com>
2017-07-18 11:30:46 +0300

    Update Icon compnent

--- src/Icon/styles.js
+++ src/Icon/styles.js
@@ -1,8 +1,8 @@
 import styled from 'styled-components'
-import { palette, size } from 'styled-theme'
-import { fromProps, toPx } from 'styled-utils'
+import { size } from 'styled-theme'
+import { bgColor, toPx } from 'styled-utils'
 
 export const IconStyle = styled.i`
-  color: ${p => palette(fromProps(p), 1, {grayscale: 0})};
+  color: ${bgColor};
   font-size: ${p => toPx(p.size) || size('iconSize')};
 `
